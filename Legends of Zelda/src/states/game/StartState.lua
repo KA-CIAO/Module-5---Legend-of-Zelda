74b220768d3c8accb5d6e9ce3615be7d09910889
ajrcastillo <72683014+ajrcastillo@users.noreply.github.com>
2020-12-24 21:58:23 +0800

    Update StartState.lua

--- Legends of Zelda/src/states/game/StartState.lua
+++ Legends of Zelda/src/states/game/StartState.lua
@@ -1,5 +1,13 @@
 StartState = Class{__includes = BaseState}
 
+function StartState:init()
+
+end
+
+function StartState:enter(params)
+
+end
+
 function StartState:update(dt)
     if love.keyboard.wasPressed('escape') then
         love.event.quit()
