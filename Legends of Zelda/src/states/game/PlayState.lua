699bd61df6dad7a7ba47a32758573888675015fc
ajrcastillo <72683014+ajrcastillo@users.noreply.github.com>
2020-12-24 22:38:29 +0800

    Update PlayState.lua

--- Legends of Zelda/src/states/game/PlayState.lua
+++ Legends of Zelda/src/states/game/PlayState.lua
@@ -20,7 +20,7 @@ function PlayState:init()
     self.player.stateMachine = StateMachine {
         ['walk'] = function() return PlayerWalkState(self.player, self.dungeon) end,
         ['idle'] = function() return PlayerIdleState(self.player) end,
-        ['swing-sword'] = function() return PlayerSwingSwordState(self.player, self.dungeon) end
+        ['swing-sword'] = function() return PlayerSwingSwordState(self.player, self.dungeon) end,
         ['pot-lifting'] = function() return PlayerPotLiftState(self.player, self.dungeon) end,
         ['pot-walking'] = function() return PlayerPotWalk(self.player, self.dungeon) end,
         ['pot-idle'] = function() return PlayerPotIdle(self.player) end,
